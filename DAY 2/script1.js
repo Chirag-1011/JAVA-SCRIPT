5470f56fa8dc78fa42954451a750d8c9a606ea81
Chirag-1011 <devmurarichirag007@gmail.com>
2024-07-18 10:11:57 +0530

    DAY 1-2

---
+++ DAY 2/script1.js
@@ -0,0 +1,121 @@
+// Q1
+
+let age=25;
+
+document.getElementById("dc1").innerHTML=`<h2>ANS:${age}</h2>`;
+
+
+// Q2
+
+let a="Alice";
+
+a="bob";
+
+document.getElementById("dc2").innerHTML=`<h2>ANS:${a}</h2>`;
+
+// Q3
+
+const PI=3.14;
+
+document.getElementById("dc3").innerHTML=`<h5>TypeError: Assignment to constant variable.</h5>`;
+
+// PI=3.1415;
+
+// document.getElementById("dc3").innerHTML=`<h2>ANS:${PI}</h2>`;
+
+
+// Q4
+
+let x=10;
+let y=20; 
+let z=x+y;
+
+document.getElementById("dc4").innerHTML=`<h2>ANS:${x} ${y} ${z}</h2>`;
+
+// Q5
+
+
+
+
+// Q6
+
+let k;
+
+console.log(k);
+
+document.getElementById("dc6").innerHTML=`<h5>ANS: undefined</h5>`;
+
+
+// Q7
+
+let d;
+
+d="NULL";
+
+
+console.log(d);
+
+document.getElementById("dc7").innerHTML=`<h5>ANS: NULL</h5>`;
+
+// Q8
+
+let e="Alice";
+
+console.log(e);
+
+document.getElementById("dc8").innerHTML=`<h5>ANS: Hello, ${e}!</h5>`;
+
+
+
+// Q9
+
+// area of a circle=pi*r*r
+
+let pi=3.14;
+let radius=5;
+
+let area=pi*radius*radius;
+
+console.log(area);
+
+document.getElementById("dc9").innerHTML=`<h5>ANS : pi=${pi}</h5>`;
+document.getElementById("dc91").innerHTML=`<h5> radius=${radius}</h5>`;
+document.getElementById("dc911").innerHTML=`<h5> area=${area}</h5>`;
+
+
+
+// Q10
+
+// area of a  rectangle=pi*r2
+
+let Length=10;
+let Width=15;
+
+let areaR=Length*Width;
+
+console.log(areaR);
+
+document.getElementById("dc10").innerHTML=`<h5>ANS : Length=${Length}</h5>`;
+document.getElementById("dc101").innerHTML=`<h5> radius=${radius}</h5>`;
+document.getElementById("dc1011").innerHTML=`<h5> areaR=${areaR}</h5>`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
