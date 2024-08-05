3cd7153ef989ce90da324ce0599b2bab9a756f28
Chirag-1011 <devmurarichirag007@gmail.com>
2024-08-05 12:08:15 +0530

    PR-3

---
+++ DAY 12/q2.js
@@ -0,0 +1,11 @@
+function dc(){
+    let a= document.getElementById("data").value
+    
+    if(a%4==0 && a%100 !=0 || a%400==0){
+        document.getElementById("ans").innerHTML=`Leap year`;
+    }
+    else(
+        document.getElementById("ans").innerHTML=`Not Leap year`
+    )
+ 
+}
